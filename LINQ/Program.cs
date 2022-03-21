a1c18274beb2f865a2be248e1064e20011f604dc
PVAK244 <khoapvade150321@fpt.edu.vn>
2022-03-21 10:56:27 +0700

    Add project files.

---
+++ LINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var products = new List<Product>(){
+                new Product(1, "Bàn trà",    400, new string[] {"Xám", "Xanh"},         2),
+                new Product(2, "Tranh treo", 400, new string[] {"Vàng", "Xanh"},        1),
+                new Product(3, "Đèn trùm",   500, new string[] {"Trắng"},               3),
+                new Product(4, "Bàn học",    200, new string[] {"Trắng", "Xanh"},       1),
+                new Product(5, "Túi da",     300, new string[] {"Đỏ", "Đen", "Vàng"},   2),
+                new Product(6, "Giường ngủ", 500, new string[] {"Trắng"},               2),
+                new Product(7, "Tủ áo",      600, new string[] {"Trắng"},               3),
+            };
+
+            var ketqua = from product in products
+                         from color in product.Colors
+                         where product.Price >= 300
+                         where color == "Đen"
+                         select product;
+
+            foreach (var product in ketqua) Console.WriteLine(product.ToString());
+
+        }
+    }
+}
